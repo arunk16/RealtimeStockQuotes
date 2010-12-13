4205fd7abdcae0a6ad98f822d0cbc903eabe025c
Arun S <arunkumar.soundrapandian@gmail.com>
2010-12-13 11:46:38 +0000

    dao test related changes.

--- Stock/src/main/java/com/mycompany/webapp/stock/dao/StockDAO.java
+++ Stock/src/main/java/com/mycompany/webapp/stock/dao/StockDAO.java
@@ -1,16 +1,12 @@
 package com.mycompany.webapp.stock.dao;
 
-import java.io.Serializable;
-import java.util.List;
-
 import org.appfuse.dao.GenericDao;
+
 import com.mycompany.webapp.stock.domain.Stock;
 
-public abstract interface StockDAO extends GenericDao<Stock, Serializable>{
+public abstract interface StockDAO extends GenericDao<Stock, Long>{
 	
-	public abstract List<Stock> getStockQuotes();
 	
-	public abstract void saveStockQuote(Stock stock);
 	
 
 }
--- Stock/src/main/java/com/mycompany/webapp/stock/dao/hibernate/StockDAOHibernate.java
+++ Stock/src/main/java/com/mycompany/webapp/stock/dao/hibernate/StockDAOHibernate.java
@@ -9,21 +9,21 @@ import org.appfuse.dao.hibernate.GenericDaoHibernate;
 import com.mycompany.webapp.stock.dao.StockDAO;
 import com.mycompany.webapp.stock.domain.Stock;
 
-public class StockDAOHibernate extends GenericDaoHibernate<Stock, Serializable> implements StockDAO{ 
+public class StockDAOHibernate extends GenericDaoHibernate<Stock, Long> implements StockDAO{ 
 
-	public StockDAOHibernate(Class<Stock> persistentClass) {
-		super(persistentClass);
+	public StockDAOHibernate() {
+		super(Stock.class);
 		// TODO Auto-generated constructor stub
 	}
-
-	public List<Stock> getStockQuotes() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void saveStockQuote(Stock stock) {
-		// TODO Auto-generated method stub
-		getHibernateTemplate().saveOrUpdate(stock);
-	}	
+//
+//	public List<Stock> getStockQuotes() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public void saveStockQuote(Stock stock) {
+//		// TODO Auto-generated method stub
+//		getHibernateTemplate().saveOrUpdate(stock);
+//	}	
 
 }
